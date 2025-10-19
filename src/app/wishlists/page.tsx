c05c858d495c866a326dfb6023bfac5eb868e91b
Jordan-182 <Jordan.P62@outlook.fr>
2025-10-19 14:57:42 +0200

    updated db schema

--- src/app/wishlists/page.tsx
+++ src/app/wishlists/page.tsx
@@ -1,7 +1,42 @@
+import CreateWishlistForm from "@/components/createWishlistForm";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { ListPlus } from "lucide-react";
+
 export default function WishlistsPage() {
   return (
     <div className="space-y-8">
-      <h1 className="text-3xl font-bold">Mes listes</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Mes listes</h1>
+        <Drawer>
+          <DrawerTrigger asChild>
+            <Button
+              className="cursor-pointer flex gap-0"
+              aria-roledescription="Ajouter un ami"
+            >
+              <ListPlus className="h-4 w-4" />
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent className="mb-20">
+            <DrawerHeader>
+              <DrawerTitle>Créer une wishlist</DrawerTitle>
+              <DrawerDescription>
+                Créez une wishlist pour vous ou l&apos;un de vos profils
+              </DrawerDescription>
+            </DrawerHeader>
+            <div className="px-4 pb-4">
+              <CreateWishlistForm />
+            </div>
+          </DrawerContent>
+        </Drawer>
+      </div>
     </div>
   );
 }
---
+++ src/components/createWishlistForm.tsx
@@ -0,0 +1,7 @@
+export default function CreateWishlistForm() {
+  return (
+    <>
+      <h2>Form pour créer une liste</h2>
+    </>
+  );
+}
